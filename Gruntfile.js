238c57651f29d8ab20e4cc30314489f3d33483a4
Andrey Klochok <andreyklochok@gmail.com>
2016-02-12 15:06:55 +0200

    Add assets files. Set up Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            css: {
+                src: [
+                    'vendor/bower/bootstrap/dist/css/bootstrap.css'
+                ],
+                dest: 'web/css/all.css'
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'web/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'web/css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        concat_sourcemap: {
+            options: {
+                sourcesContent: true
+            },
+            all: {
+                files: {
+                    'web/js/all.js': grunt.file.readJSON('assets/js/all.json')
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['vendor/bower/bootstrap/fonts/*'],
+                        dest: 'web/fonts/',
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            lib: {
+                files: {
+                    'web/js/lib.min.js': 'web/js/lib.js'
+                }
+            },
+            all: {
+                files: {
+                    'web/js/all.min.js': 'web/js/all.js'
+                }
+            }
+        },
+        //imagemin: {
+        //    dynamic: {
+        //        files: [{
+        //            expand: true,
+        //            cwd: 'web/img/',
+        //            src: ['**/*.{png,jpg,gif}'],
+        //            dest: 'web/img/'
+        //        }]
+        //    }
+        //},
+        watch: {
+            js: {
+                files: ['assets/js/**/*.js', 'assets/js/all.json'],
+                tasks: ['concat_sourcemap', 'uglify:lib'],
+                options: {
+                    livereload: true
+                }
+            },
+            fonts: {
+                files: [
+                    'vendor/bower/bootstrap/fonts/*'
+                ],
+                tasks: ['copy'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    // Plugin loading
+    grunt.loadNpmTasks('grunt-concat-sourcemap');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    //grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // Task definition
+    grunt.registerTask('build', ['concat', 'cssmin', 'copy', 'concat_sourcemap', 'uglify']); // 'imagemin',
+    grunt.registerTask('default', ['watch']);
+};
