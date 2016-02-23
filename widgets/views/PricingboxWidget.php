f1f86bae03e18e6f08fa6d0f085526abe00711ff
Andrey Klochok <andreyklochok@gmail.com>
2016-02-23 13:33:05 +0200

    Promotioanal offers

--- widgets/views/PricingboxWidget.php
+++ widgets/views/PricingboxWidget.php
@@ -2,69 +2,45 @@
 <div class="pricingbox no-image-bg">
     <div class="row">
         <div class="col-sm-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="200ms">
-            <h3>MOST POPULAR HOSTING PLANS</h3>
+            <h3><?= Yii::t('app', 'PROMOTIONAL OFFERS')?></h3>
             <div class="titleborder dark centered">
                 <div class="titleborder_left"></div>
                 <div class="titleborder_sign"></div>
                 <div class="titleborder_right"></div>
             </div>
-            <p>Transformative donors, public sector marginalized communities maintain giving combat malaria civic engagement</p>
+            <p></p>
         </div>
     </div>
 
     <div class="row spacing-25">
 
-        <div class="col-sm-4">
+        <div class="col-sm-4 col-sm-offset-2">
             <div class="pricing-plan red wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="200ms">
-                <div class="pricing-title">SHARED HOSTING</div>
-                <div class="pricing-price"><div class="starting">STARTING AT</div>$9.99<span>/m</span></div>
+                <div class="pricing-title"><?= Yii::t('app', 'Premium package') ?></div>
+                <div class="pricing-price">$0.95<span>/m</span></div>
                 <div class="pricing-features">
                     <ul>
-                        <li>2GB RAM</li>
-                        <li>20GB SSD Cloud Storage</li>
-                        <li>Weekly Backups</li>
-                        <li>DDoS Protection</li>
-                        <li>Full Root Access</li>
-                        <li>24/7/365 Tech Support</li>
+                        <li><?= Yii::t('app', '3 independent DNS servers in the Netherlands and United States') ?></li>
+                        <li><?= Yii::t('app', 'operation with different types of records: A, AAAA, MX, CNAME, TXT, NS') ?></li>
+                        <li><?= Yii::t('app', 'opportunity to work with subdomains') ?></li>
+                        <li><?= Yii::t('app', 'presets for google apps') ?></li>
                     </ul>
                 </div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)">ORDER NOW</a></div>
+                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)"><?= Yii::t('app', 'More details') ?></a></div>
             </div>
         </div>
 
         <div class="col-sm-4">
             <div class="pricing-plan purple wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">
-                <div class="pricing-title">CLOUD VPS</div>
-                <div class="pricing-price"><div class="starting">STARTING AT</div>$39.99<span>/m</span></div>
+                <div class="pricing-title"><?= Yii::t('app', 'Whois Protect free') ?></div>
+                <div class="pricing-price">$0</div>
                 <div class="pricing-features">
                     <ul>
-                        <li>4GB RAM</li>
-                        <li>50GB SSD Cloud Storage</li>
-                        <li>Weekly Backups</li>
-                        <li>DDoS Protection</li>
-                        <li>Full Root Access</li>
-                        <li>24/7/365 Tech Support</li>
+                        <li><?= Yii::t('app', 'personal data protection against spam, information gathering, direct calls') ?></li>
+                        <li><?= Yii::t('app', 'function of email forwarding') ?></li>
                     </ul>
                 </div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)">DEPLOY NOW</a></div>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div class="pricing-plan blue wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="600ms">
-                <div class="pricing-title">DEDICATED SERVERS</div>
-                <div class="pricing-price"><div class="starting">STARTING AT</div>$109.99<span>/m</span></div>
-                <div class="pricing-features">
-                    <ul>
-                        <li>8GB RAM</li>
-                        <li>90GB SSD Cloud Storage</li>
-                        <li>Weekly Backups</li>
-                        <li>DDoS Protection</li>
-                        <li>Full Root Access</li>
-                        <li>24/7/365 Tech Support</li>
-                    </ul>
-                </div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)">SIGN UP</a></div>
+                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)"><?= Yii::t('app', 'More details') ?></a></div>
             </div>
         </div>
 
