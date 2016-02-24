0f18537b9010cc35ea04a3f02e0898f7400a483a
Andrey Klochok <andreyklochok@gmail.com>
2016-02-24 16:36:43 +0200

    Add Hosting scaffolding

--- controllers/HostingController.php
+++ controllers/HostingController.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tofid
- * Date: 24.02.16
- * Time: 16:23
- */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class Hosting extends Controller
+{
+    public function actionXenSsd()
+    {
+        return $this->render('xen_ssd');
+    }
+
+    public function actionOpenVz()
+    {
+        return $this->render('open_vz');
+    }
+
+    public function actionTariffsDetail()
+    {
+        return $this->render('tariffs_detail');
+    }
+
+    public function actionAdvantages()
+    {
+        return $this->render('advantages');
+    }
+
+    public function actionWhatIsVds()
+    {
+        return $this->render('what_is_vds');
+    }
+}
