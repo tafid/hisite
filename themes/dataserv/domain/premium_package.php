81aababba6328466a3caecf41a1c3d0917224970
Andrey Klochok <andreyklochok@gmail.com>
2016-02-25 19:04:44 +0200

    Premium package page

--- themes/dataserv/domain/premium_package.php
+++ themes/dataserv/domain/premium_package.php
@@ -37,7 +37,6 @@ $this->blocks['subHeaderClass'] = 'dedicated-servers';
                         <li><span class="pull-right"><i class="fa fa-times text-danger"></i></span><?= Yii::t('app', 'Domain parking') ?></li>
                     </ul>
                 </div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)">ORDER NOW</a></div>
             </div>
         </div>
 
@@ -59,10 +58,41 @@ $this->blocks['subHeaderClass'] = 'dedicated-servers';
                         <li><span class="pull-right"><i class="fa fa-check text-success"></i></span><?= Yii::t('app', 'Domain parking') ?></li>
                     </ul>
                 </div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple" href="javascript:void(0)">ORDER NOW</a></div>
             </div>
         </div>
 
     </div>
 </div>
 <!-- END OF PRICE BOXES -->
+
+<div class="shared-table">
+    <div class="difference">
+        <div class="row spacing-25">
+
+            <div class="col-md-9 col-sm-12 center-block">
+                <div class="centered">
+                    <p></p><p><b>Domain</b> – an alphanumeric name that uniquely identifies a particular website.</p>
+                    <p><b>DNS</b> – a network service that provides information about domains. Each computer connected to the Internet has a unique IP-address, made up of many numbers that are difficult to memorize. A Domain Name System (DNS) allows you to replace the numbers with a description of your choice, making it easier to find the correct one.</p>
+                    <p><b>А</b> – a record that binds the hostname to an IP-address where the resource is located.</p>
+                    <p><b>АААА</b> – a similar record that uses the address of the new Pv6 standard.</p>
+                    <p><b>CNAME</b> – the canonical name or "alias" that is used to redirect requests from one domain name to another.</p>
+                    <p><b>MX</b> – a record that points to the IP-address of the mail server of a domain.</p>
+                    <p><b>NS</b> – indicates the IP-address of the DNS-server for this domain.</p>
+                    <p><b>TXT</b> – indicates the IP-address that is used by the DNS-server of a domain.</p>
+                    <p><b>SOA</b> – a parameter that specifies which server stores the reference entry for your domain, stores the contact information and the caching information about the DNS-server interaction.</p><p></p>
+
+                </div>
+            </div>
+
+            <div class="col-md-9 col-sm-12 center-block md-mt-50">
+                <h3 class="text-center">Terms of Use for "Premium Package".</h3>
+                <div class="newplans_box"></div>
+                <p class="text-center">
+                    Please note that if you have previously activated the "Premium Package", but do not want to pay for this feature, all previously made settings for domains will remain and will continue to function normally, but you will not be able to change or delete records.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->render('_domainFooter'); ?>
