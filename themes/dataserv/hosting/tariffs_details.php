a5efe22bdb1b8c5fdba3fa313847e0463a957419
Andrey Klochok <andreyklochok@gmail.com>
2016-02-26 19:40:26 +0200

    minor

---
+++ themes/dataserv/hosting/tariffs_details.php
@@ -0,0 +1,4 @@
+<?php
+$this->title = Yii::t('app', 'Tariffs details');
+$this->blocks['subTitle'] = Yii::t('app', 'Hosting by AHnames is an optimal and reliable solution for any project at affordable price');
+?>
