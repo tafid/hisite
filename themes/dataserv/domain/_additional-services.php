b44fdc0a704a0f86a563182827e01111f93c03c7
Andrey Klochok <andreyklochok@gmail.com>
2016-02-26 18:38:06 +0200

    minor fixes in _additional-services

--- themes/dataserv/domain/_additional-services.php
+++ themes/dataserv/domain/_additional-services.php
@@ -1,25 +1,25 @@
 <div class="domain-features">
     <div class="row">
         <div class="col-sm-4">
-            <div class="contactmethod darkgray">
-                <h5><?= Yii::t('app', 'Android App') ?></h5>
-                <p>Times democracy change movements process medicine. Sustainable future.</p>
+            <div class="contactmethod">
+                <h5 class="text-center"><?= Yii::t('app', 'Android App') ?></h5>
+                <div class="circle"><i class="fa fa-android"></i></div>
                 <div class="text-center"><a class="mtr-btn button-raised ripple has-ripple" href="javascript:void(0)"><?= Yii::t('app', 'View more') ?></a></div>
             </div>
         </div>
 
         <div class="col-sm-4">
-            <div class="contactmethod blue">
-                <h5><?= Yii::t('app', 'Whois lookup') ?></h5>
-                <p>Women and children eradicate Millennium Development Goals peaceful.</p>
+            <div class="contactmethod">
+                <h5 class="text-center"><?= Yii::t('app', 'Whois lookup') ?></h5>
+                <div class="circle"><i class="fa fa-question"></i></div>
                 <div class="text-center"><a class="mtr-btn button-raised ripple has-ripple" href="javascript:void(0)"><?= Yii::t('app', 'View more') ?></a></div>
             </div>
         </div>
 
         <div class="col-sm-4">
-            <div class="contactmethod green">
-                <h5><?= Yii::t('app', 'IOS App') ?></h5>
-                <p>Involvement. Reproductive rights, vulnerable population raise awareness.</p>
+            <div class="contactmethod">
+                <h5 class="text-center"><?= Yii::t('app', 'IOS App') ?></h5>
+                <div class="circle"><i class="fa fa-apple"></i></div>
                 <div class="text-center"><a class="mtr-btn button-raised ripple has-ripple" href="javascript:void(0)"><?= Yii::t('app', 'View more') ?></a></div>
             </div>
         </div>
