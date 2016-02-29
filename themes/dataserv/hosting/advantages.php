bcda40235394bec0117c967dbca37400adc88963
Andrey Klochok <andreyklochok@gmail.com>
2016-02-29 19:14:50 +0200

    add new page

--- themes/dataserv/hosting/advantages.php
+++ themes/dataserv/hosting/advantages.php
@@ -1,3 +1,39 @@
 <?php
-$this->title = Yii::t('app', '');
+$this->title = Yii::t('app', 'Advantages');
+$this->blocks['subTitle'] = Yii::t('app', 'Hosting by AHnames is an optimal and reliable solution for any project at affordable price');
 ?>
+
+<!-- FEATURES -->
+<div class="various">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="row wow fadeInRight" data-wow-duration="500ms" data-wow-delay="200ms">
+                <div class="col-sm-3 col-md-2"><i class="fa fa-shield"></i></div>
+                <div class="col-sm-9 col-md-10"><h5><?= Yii::t('app', 'Round-the-clock monitoring') ?></h5>
+                    <p><?= Yii::t('app', 'For a timely response we monitor all of our servers 24/7.') ?></p>
+                </div>
+            </div>
+            <div class="row wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
+                <div class="col-sm-3 col-md-2"><i class="fa fa-dot-circle-o"></i></div>
+                <div class="col-sm-9 col-md-10"><h5><?= Yii::t('app', 'Equipment from the top manufacturers') ?></h5>
+                    <p><?= Yii::t('app', 'All of our servers are built using the equipment from the top manufacturers.') ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="row wow fadeInRight" data-wow-duration="500ms" data-wow-delay="400ms">
+                <div class="col-sm-3 col-md-2"><i class="fa fa-code-fork"></i></div>
+                <div class="col-sm-9 col-md-10"><h5><?= Yii::t('app', 'Expert technical support') ?></h5>
+                    <p><?= Yii::t('app', 'Our team of professionals will solve any problem in a timely manner.') ?></p>
+                </div>
+            </div>
+            <div class="row wow fadeInRight" data-wow-duration="500ms" data-wow-delay="500ms">
+                <div class="col-sm-3 col-md-2"><i class="fa fa-compass"></i></div>
+                <div class="col-sm-9 col-md-10"><h5><?= Yii::t('app', 'Optimal solutions for every project') ?></h5>
+                    <p><?= Yii::t('app', 'We can help you find the best solution for your problem.') ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END OF FEATURES -->
