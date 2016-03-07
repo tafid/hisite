5f6184c1661040e1f2e08f5fb2143e94e02b4fe5
Andrey Klochok <andreyklochok@gmail.com>
2016-03-07 15:06:26 +0200

    Add translation function

--- themes/dataserv/site/page/rules.php
+++ themes/dataserv/site/page/rules.php
@@ -10,10 +10,13 @@ $this->registerCss(".vps-features-tabs .tabs-left-vertical > .nav-tabs > li > a 
             <div class="tabbable tabs-left-vertical" data-wow-duration="1000ms" data-wow-delay="300ms">
 
                 <ul class="nav nav-tabs">
-                    <li class="active"><a href="#termsOfUse" data-toggle="tab"><?= Yii::t('app', 'Domains terms of use') ?></a></li>
+                    <li class="active"><a href="#termsOfUse"
+                                          data-toggle="tab"><?= Yii::t('app', 'Domains terms of use') ?></a></li>
                     <li><a href="#privacyPolicy" data-toggle="tab"><?= Yii::t('app', 'Privacy Policy') ?></a></li>
-                    <li><a href="#cancelationPolicy" data-toggle="tab"><?= Yii::t('app', 'Cancelation policy') ?></a></li>
-                    <li><a href="#domainRemovalAndAutoRenewalPolicy" data-toggle="tab"><?= Yii::t('app', 'Domain removal and auto renewal Policy') ?></a></li>
+                    <li><a href="#cancelationPolicy" data-toggle="tab"><?= Yii::t('app', 'Cancelation policy') ?></a>
+                    </li>
+                    <li><a href="#domainRemovalAndAutoRenewalPolicy"
+                           data-toggle="tab"><?= Yii::t('app', 'Domain removal and auto renewal Policy') ?></a></li>
                 </ul>
 
                 <div class="tab-content">
@@ -39,7 +42,7 @@ $this->registerCss(".vps-features-tabs .tabs-left-vertical > .nav-tabs > li > a 
                     <div class="tab-pane fade" id="domainRemovalAndAutoRenewalPolicy">
                         <h4><?= Yii::t('app', 'Domain removal and auto renewal Policy') ?></h4>
                         <hr class="small"/>
-                        <p class="subtitle">Capacity building insurmountable challenges, Andrew Carnegie rural development</p>
+                        <p class="subtitle"><?= Yii::t('app', 'Capacity building insurmountable challenges, Andrew Carnegie rural development') ?></p>
                         <?= $this->render('_removal_and_auto_renewal'); ?>
                     </div>
 
@@ -51,5 +54,7 @@ $this->registerCss(".vps-features-tabs .tabs-left-vertical > .nav-tabs > li > a 
     </div>
 </div>
 
-<a href="https://www.icann.org/resources/pages/responsibilities-2014-03-14-en" target="_blank"  data-toggle="tab"><i class="fa fa-check-square-o lightblue"></i><?= Yii::t('app', 'Registrant rights and responsibilities') ?></a>
-<a href="http://afilias.info/policies" target="_blank" data-toggle="tab"><i class="fa fa-gratipay orange"></i><?= Yii::t('app', 'Afilias Domain Anti-Abuse Policy') ?></a>
+<a href="https://www.icann.org/resources/pages/responsibilities-2014-03-14-en" target="_blank" data-toggle="tab"><i
+        class="fa fa-check-square-o lightblue"></i><?= Yii::t('app', 'Registrant rights and responsibilities') ?></a>
+<a href="http://afilias.info/policies" target="_blank" data-toggle="tab"><i
+        class="fa fa-gratipay orange"></i><?= Yii::t('app', 'Afilias Domain Anti-Abuse Policy') ?></a>
