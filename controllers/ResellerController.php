04ef820d04adeed36fd519bc146b4d0b36125038
Andrey Klochok <andreyklochok@gmail.com>
2016-03-14 19:10:32 +0200

    minor

--- controllers/ResellerController.php
+++ controllers/ResellerController.php
@@ -10,14 +10,14 @@ class ResellerController extends Controller
 {
     public function actionPrices()
     {
-        $list = self::make_sub(self::make([2013590,2013498,2013544]), 'id');
+        $list = self::make_sub(self::make([2013590, 2013498, 2013544]), 'id');
         $tariffs = Tariff::perform('GetInfo', $list, true);
         $zones = Domain::perform('GetZones', [], true);
 
         foreach ($tariffs as $id => $v) {
-            $range = substr($v['name'],15);
+            $range = substr($v['name'], 15);
             if (!$range) $range = '0';
-            else $range = preg_replace('/\D/','',$range);
+            else $range = preg_replace('/\D/', '', $range);
             $info[$range] = self::domain($tariffs[$id]['resources'], $zones);
         };
 
@@ -30,28 +30,6 @@ class ResellerController extends Controller
         return $this->render('prices', compact('info', 'zones', 'tariffOptions'));
     }
 
-    public static function domain ($resources, $zones) {
-        foreach ($resources as $k => $v) {
-            if ($zones[$v['object_id']]) $resource['zone:.'.$zones[$v['object_id']]][$v['type']] = $v;
-            if (preg_match('/^premium_dns/', $v['type'])) $resource['ref:class,feature'][$v['type']] = $v;
-        }
-
-        return $resource;
-    }
-
-    public static function make ($keys,$vals=null) {
-        if (is_null($vals)) $vals = $keys;
-        foreach ($keys as $n=>$key) $res[$key] = is_array($vals) ? $vals[$n] : $vals;
-        return $res;
-    }
-
-    public static function make_sub ($array,$v_key,$k_key=null) {
-        $res = array();
-        foreach ($array as $k=>$v) $res[$k][$v_key] = $v;
-        if ($k_key) foreach ($array as $k=>$v) $res[$k][$k_key] = $k;
-        return $res;
-    }
-
     public function actionAdvantages()
     {
         return $this->render('advantages');
@@ -60,5 +38,30 @@ class ResellerController extends Controller
     public function actionApi()
     {
         return $this->render('api');
+    }
+    // todo: delete this method
+    public static function domain($resources, $zones)
+    {
+        foreach ($resources as $k => $v) {
+            if ($zones[$v['object_id']]) $resource['zone:.' . $zones[$v['object_id']]][$v['type']] = $v;
+            if (preg_match('/^premium_dns/', $v['type'])) $resource['ref:class,feature'][$v['type']] = $v;
+        }
+
+        return $resource;
+    }
+    // todo: delete this method
+    public static function make($keys, $vals = null)
+    {
+        if (is_null($vals)) $vals = $keys;
+        foreach ($keys as $n => $key) $res[$key] = is_array($vals) ? $vals[$n] : $vals;
+        return $res;
+    }
+    // todo: delete this method
+    public static function make_sub($array, $v_key, $k_key = null)
+    {
+        $res = array();
+        foreach ($array as $k => $v) $res[$k][$v_key] = $v;
+        if ($k_key) foreach ($array as $k => $v) $res[$k][$k_key] = $k;
+        return $res;
     }
 }
