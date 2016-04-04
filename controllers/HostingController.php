4847c54303318b5d3d31f157e9c07a49e7bb4c26
Andrey Klochok <andreyklochok@gmail.com>
2016-04-04 17:52:58 +0300

    minor

--- controllers/HostingController.php
+++ controllers/HostingController.php
@@ -8,34 +8,34 @@ use hipanel\modules\finance\models\Tariff;
 use hipanel\modules\stock\models\Part;
 use Yii;
 use yii\base\Exception;
+use yii\helpers\Html;
 use yii\web\Controller;
 
 class HostingController extends Controller
 {
-    static public function cjoin ($array, $delimiter=',') {
+    static public function cjoin($array, $delimiter = ',')
+    {
         if (!is_array($array)) return $array;
         $res = array(); /// TODO redo without created array - just concatenate string for efficiency
         foreach ($array as $a) {
             $b = trim($a);
-            if ($b) array_push($res,$b);
+            if ($b) array_push($res, $b);
         };
-        return join($delimiter,$res);
+        return join($delimiter, $res);
     }
 
 
-    private function getAllData()
-    {
-
-    }
-
-    public function actionXenSsd()
+    /**
+     * @param $type (svds|ovds)
+     * @return mixed
+     */
+    private function packages($type)
     {
         $parts = [];
-        $availablePackage = Yii::$app->params['vdsproduct'];
         $_pkgs = Tariff::find(['scenario' => 'get-available-info'])
             ->joinWith('resources')
-            ->andFilterWhere(['type' => is_array($availablePackage) ? self::cjoin(array_keys($availablePackage),',') : $availablePackage])
-            ->andFilterWhere(['seller' => Yii::$app->params['seller']])
+            ->andWhere(['type' => $type])
+            ->andWhere(['seller' => Yii::$app->params['seller']])
             ->all();
         foreach ($_pkgs as $key => $v) {
             foreach ($v['resources'] as $value) {
@@ -53,31 +53,44 @@ class HostingController extends Controller
         $j = 0.01;
         foreach ($_pkgs as $key => $v) {
             try {
-                $_pkgs[$key]['resources'] = ResourcesHelper::server($v['resources'], $parts);
-                $_pkgs[$key]['price'] = $prices[$key]['value']['usd']['price'];
-                $_pkgs[$key]['value'] = $prices[$key]['value']['usd']['value'];
+                $_pkgs[$key]->resources = ResourcesHelper::server($v['resources'], $parts);
+                $_pkgs[$key]->price = $prices[$key]['value']['usd']['price'];
+                $_pkgs[$key]->value = $prices[$key]['value']['usd']['value'];
             } catch (Exception $e) {
                 unset($_pkgs[$key]);
             }
-            unset($_pkgs[$key]);
             $t = 0.00;
             $existPrice = false;
             while ($t <= $j) {
-                if ($vs[($_pkgs[$key]['value'] + $t) * 100]) $existPrice = true;
+                if ($vs[($_pkgs[$key]->value + $t) * 100]) $existPrice = true;
                 $t += 0.01;
             }
-            if (!$_pkgs[$key]['value'] || $existPrice) $j += 0.01;
-            $vs[($_pkgs[$key]['value'] + $j) * 100] = $key;
+            if (!$_pkgs[$key]->value || $existPrice) $j += 0.01;
+            $vs[($_pkgs[$key]->value + $j) * 100] = $key;
         }
         ksort($vs);
-        foreach ($vs as $id) $packages["0$id"] = $_pkgs[$id];
+        foreach ($vs as $id)
+            $packages["0$id"] = $_pkgs[$id];
 
-        return $this->render('xen_ssd', compact('packages', 'tariffTypes'));
+        return $packages;
+    }
+
+    public function actionXenSsd()
+    {
+        return $this->render('xen_ssd', [
+            'packages' => $this->packages('svds'),
+            'tariffTypes' => Yii::$app->params['vdsproduct'],
+            'testVDSPurchased' => ['id' => 1]
+        ]);
     }
 
     public function actionOpenVz()
     {
-        return $this->render('open_vz');
+        return $this->render('xen_ssd', [
+            'packages' => $this->packages('ovds'),
+            'tariffTypes' => Yii::$app->params['vdsproduct'],
+            'testVDSPurchased' => ['id' => 1]
+        ]);
     }
 
     public function actionTariffsDetails()
@@ -93,5 +106,46 @@ class HostingController extends Controller
     public function actionWhatIsVds()
     {
         return $this->render('what_is_vds');
+    }
+
+    /**
+     * @param $type (svds|ovds)
+     * @return array
+     */
+    public function priceAttributeData($type)
+    {
+        return [
+            'cpu' =>[
+                'label' => Yii::t('app', 'CPU'),
+                'buildValue' => function ($resource) {
+                    return $resource['cpu']['partno'];
+                }
+            ],
+            'ram' =>[
+                'label' => Yii::t('app', 'RAM'),
+                'tooltip' => $type == 'svds' ? Yii::t('app', 'Additional $4/1 GB') : Yii::t('app', 'Additional $4/1 GB')
+            ],
+            'hd' =>[
+                'label' => $type == 'svds' ? Yii::t('app', 'SSD') : Yii::t('app', 'HDD+SSD cache'),
+            ],
+            'dedicated_ip' =>[
+                'label' => Yii::t('app', 'Dedicated IP'),
+            ],
+            'support' =>[
+                'label' => Html::a(Yii::t('app', 'Support 24/7'), '#'),
+            ],
+            'traffic' =>[
+                'label' => Yii::t('app', 'Traffic'),
+            ],
+            'port' =>[
+                'label' => Yii::t('app', 'Connection Port'),
+            ],
+            'control_panel' =>[
+                'label' => Html::a(Yii::t('app', 'Control panel'), '#'),
+            ],
+            'purpose' =>[
+                'label' => Yii::t('app', 'Purpose'),
+            ],
+        ];
     }
 }
--- helpers/ResourcesHelper.php
+++ helpers/ResourcesHelper.php
@@ -9,39 +9,47 @@ class ResourcesHelper
      * @param $zones
      * @return mixed
      */
-    public static function domain ($resources, $zones) {
+    public static function domain($resources, $zones)
+    {
         foreach ($resources as $k => $v) {
-            if ($zones[$v['object_id']]) $resource['zone:.'.$zones[$v['object_id']]][$v['type']] = $v;
+            if ($zones[$v['object_id']]) $resource['zone:.' . $zones[$v['object_id']]][$v['type']] = $v;
             if (preg_match('/^premium_dns/', $v['type'])) $resource['ref:class,feature'][$v['type']] = $v;
         }
 
         return $resource;
     }
 
-    public static function server ($resources, $models, $profile_to_model = null) {
+    /**
+     * @param $resources
+     * @param $models
+     * @param null $profile_to_model
+     * @return mixed
+     */
+    public static function server($resources, $models, $profile_to_model = null)
+    {
         if (!$profile_to_model) {
             foreach ($resources as $resource) {
-                if ($resource['partno']) $resource['partno'] = trim(str_ireplace(['xen','openvz','cpu','ram','hdd','ssd'],'',$resource['partno']));
+                if ($resource['partno']) $resource['partno'] = trim(str_ireplace(['xen', 'openvz', 'cpu', 'ram', 'hdd', 'ssd'], '', $resource['partno']));
                 if ($models[$resource['object_id']]) {
                     $resource['partno'] = $models[$resource['object_id']]['partno'];
-                    $resource['partno'] = trim(str_ireplace(['xen','openvz','cpu','ram','hdd','ssd'],'',$resource['partno']));
+                    $resource['partno'] = trim(str_ireplace(['xen', 'openvz', 'cpu', 'ram', 'hdd', 'ssd'], '', $resource['partno']));
                     $resource['model_type'] = $models[$resource['object_id']]['model_type'];
                 }
-                $_resources[$resource['model_type'] ? : $resource['type']] = $resource;
+                $_resources[$resource['model_type'] ?: $resource['type']] = $resource;
             }
         } else {
-            foreach($resources as $resource) {
-                if ($resource['partno']) $resource['partno'] = trim(str_ireplace(['xen','openvz','cpu','ram','hdd','ssd'],'',$resource['partno']));
+            foreach ($resources as $resource) {
+                if ($resource['partno']) $resource['partno'] = trim(str_ireplace(['xen', 'openvz', 'cpu', 'ram', 'hdd', 'ssd'], '', $resource['partno']));
                 $model_id = $profile_to_model[$resource['object_id']];
                 if (array_key_exists($resource['object_id'], $profile_to_model)) {
                     $resource['partno'] = $models[$model_id]['partno'];
-                    $resource['partno'] = trim(str_ireplace(['xen','openvz','cpu','ram','hdd','ssd'],'',$resource['partno']));
+                    $resource['partno'] = trim(str_ireplace(['xen', 'openvz', 'cpu', 'ram', 'hdd', 'ssd'], '', $resource['partno']));
                     $resource['model_type'] = $models[$model_id]['type_name'];
                 }
-                $_resources[$resource['model_type'] ? : $resource['type']] = $resource;
+                $_resources[$resource['model_type'] ?: $resource['type']] = $resource;
             }
-
         }
+
         return $_resources;
     }
 }
