9738c80841e08a27601ee2d96f1c81fb2b411c21
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-04-16 20:10:12 +0300

    removed unused params section

--- config/params.php
+++ config/params.php
@@ -3,15 +3,6 @@
 return [
     'adminEmail' => 'admin@example.com',
     'seller' => 'ahnames',
-    'product' => [
-        'domain' => '{Lang:Domain}',
-        'svds' => '{Lang:Xen VDS + SSD}',
-        'ovds' => '{Lang:Open VZ}',
-        'server' => '{Lang:server}',
-//        'avds'              => '{Lang:Xen VDS}',
-//        'grouping'          => '{Lang:Grouping}',
-        'resources' => '{Lang:Resources}',
-    ],
     'api_url' => 'https://hiapi.ahnames.com',
     'currency' => 'USD',
 ];
