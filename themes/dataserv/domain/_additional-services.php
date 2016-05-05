eba9c613f756ec5bfb0a802483b000f3a98abf8d
tofid <andreyklochok@gmail.com>
2016-05-05 19:27:37 +0300

    Change view additional-services

--- themes/dataserv/domain/_additional-services.php
+++ themes/dataserv/domain/_additional-services.php
@@ -2,75 +2,27 @@
 use yii\helpers\Html;
 
 ?>
-
-<!--div class="domain-features">
-    <div class="row">
-        <div class="col-sm-4">
-            <div class="contactmethod">
-                <h5 class="text-center"><?= Yii::t('app', 'Android App') ?></h5>
-                <div class="circle"><i class="fa fa-android"></i></div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple has-ripple" href="javascript:void(0)"><?= Yii::t('app', 'View more') ?></a></div>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div class="contactmethod">
-                <h5 class="text-center"><?= Yii::t('app', 'Whois lookup') ?></h5>
-                <div class="circle"><i class="fa fa-question"></i></div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple has-ripple" href="javascript:void(0)"><?= Yii::t('app', 'View more') ?></a></div>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div class="contactmethod">
-                <h5 class="text-center"><?= Yii::t('app', 'IOS App') ?></h5>
-                <div class="circle"><i class="fa fa-apple"></i></div>
-                <div class="text-center"><a class="mtr-btn button-raised ripple has-ripple" href="javascript:void(0)"><?= Yii::t('app', 'View more') ?></a></div>
-            </div>
-        </div>
-    </div>
-</div-->
-
-<!--div class="introcolumns">
-    <div class="row">
-        <div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: fadeInUp;">
-            <div class="circle"><i class="fa fa-android"></i></div>
-            <h4><?= Html::a(Yii::t('app', 'Android App'), '#') ?></h4>
-        </div>
-
-        <div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 600ms; animation-name: fadeInUp;">
-            <div class="circle"><i class="fa fa-search" aria-hidden="true"></i></div>
-            <h4><?= Yii::t('app', 'Whois lookup') ?></h4>
-        </div>
-
-        <div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="900ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 900ms; animation-name: fadeInUp;">
-            <div class="circle"><i class="fa fa-apple"></i></div>
-            <h4><?= Yii::t('app', 'IOS App') ?></h4>
-        </div>
-
-    </div>
-</div-->
 <div class="domaintlds">
     <div class="row">
         <div class="block-grid-sm-3 block-grid-xs-1 domain-tlds-grid">
             <div class="block-grid-item wow zoomIn" data-wow-duration="500ms" data-wow-delay="100ms" style="visibility: visible; animation-duration: 500ms; animation-delay: 100ms; animation-name: zoomIn;">
-                <div class="tld">
+                <div class="tld introcolumns">
                     <div class="circle"><i class="fa fa-android"></i></div>
-                    <div class="tldprice"><?= Yii::t('app', 'Android App') ?></div>
+                    <div class="tldprice"><?= Html::a(Yii::t('app', 'Get Android App'), '#', ['class' => 'mtr-btn button-fab ripple btn-sm has-ripple']) ?></div>
                 </div>
             </div>
 
             <div class="block-grid-item wow zoomIn" data-wow-duration="500ms" data-wow-delay="150ms" style="visibility: visible; animation-duration: 500ms; animation-delay: 150ms; animation-name: zoomIn;">
-                <div class="tld">
+                <div class="tld introcolumns">
                     <div class="circle"><i class="fa fa-search" aria-hidden="true"></i></div>
-                    <div class="tldprice"><?= Yii::t('app', 'Whois lookup') ?></div>
+                    <div class="tldprice"><?= Html::a(Yii::t('app', 'Whois lookup'), '#', ['class' => 'mtr-btn button-fab ripple btn-sm has-ripple']) ?></div>
                 </div>
             </div>
 
             <div class="block-grid-item wow zoomIn" data-wow-duration="500ms" data-wow-delay="200ms" style="visibility: visible; animation-duration: 500ms; animation-delay: 200ms; animation-name: zoomIn;">
-                <div class="tld">
+                <div class="tld introcolumns">
                     <div class="circle"><i class="fa fa-apple"></i></div>
-                    <div class="tldprice"><?= Yii::t('app', 'IOS App') ?></div>
+                    <div class="tldprice"><?= Html::a(Yii::t('app', 'Get IOS App'), '#', ['class' => 'mtr-btn button-fab ripple btn-sm has-ripple']) ?></div>
                 </div>
             </div>
 
